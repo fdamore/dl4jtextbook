d604e0ac70504cc8af0c6e7a2bc041d452ab6ee1
francesco <francesco@darkstar>
2018-11-20 14:11:00 +0100

    Set some syntax and study nets

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/paragraphvectors/ParagraphVectorsClassifierExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/paragraphvectors/ParagraphVectorsClassifierExample.java
@@ -1,5 +1,8 @@
 package org.deeplearning4j.examples.nlp.paragraphvectors;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
 import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.berkeley.Pair;
 import org.deeplearning4j.examples.nlp.paragraphvectors.tools.LabelSeeker;
@@ -17,118 +20,113 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileNotFoundException;
-import java.util.List;
-
 /**
- * This is basic example for documents classification done with DL4j ParagraphVectors.
- * The overall idea is to use ParagraphVectors in the same way we use LDA:
- * topic space modelling.
+ * This is basic example for documents classification done with DL4j
+ * ParagraphVectors. The overall idea is to use ParagraphVectors in the same way
+ * we use LDA: topic space modelling.
  *
- * In this example we assume we have few labeled categories that we can use
- * for training, and few unlabeled documents. And our goal is to determine,
- * which category these unlabeled documents fall into
+ * In this example we assume we have few labeled categories that we can use for
+ * training, and few unlabeled documents. And our goal is to determine, which
+ * category these unlabeled documents fall into
  *
  *
- * Please note: This example could be improved by using learning cascade
- * for higher accuracy, but that's beyond basic example paradigm.
+ * Please note: This example could be improved by using learning cascade for
+ * higher accuracy, but that's beyond basic example paradigm.
  *
  * @author raver119@gmail.com
  */
 public class ParagraphVectorsClassifierExample {
 
-    ParagraphVectors paragraphVectors;
-    LabelAwareIterator iterator;
-    TokenizerFactory tokenizerFactory;
-
     private static final Logger log = LoggerFactory.getLogger(ParagraphVectorsClassifierExample.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
 
-      ParagraphVectorsClassifierExample app = new ParagraphVectorsClassifierExample();
-      app.makeParagraphVectors();
-      app.checkUnlabeledData();
-        /*
-                Your output should be like this:
-
-                Document 'health' falls into the following categories:
-                    health: 0.29721372296220205
-                    science: 0.011684473733853906
-                    finance: -0.14755302887323793
-
-                Document 'finance' falls into the following categories:
-                    health: -0.17290237675941766
-                    science: -0.09579267574606627
-                    finance: 0.4460859189453788
-
-                    so,now we know categories for yet unseen documents
-         */
+	final ParagraphVectorsClassifierExample app = new ParagraphVectorsClassifierExample();
+	app.makeParagraphVectors();
+	app.checkUnlabeledData();
+	/*
+	 * Your output should be like this:
+	 * 
+	 * Document 'health' falls into the following categories: health:
+	 * 0.29721372296220205 science: 0.011684473733853906 finance:
+	 * -0.14755302887323793
+	 * 
+	 * Document 'finance' falls into the following categories: health:
+	 * -0.17290237675941766 science: -0.09579267574606627 finance:
+	 * 0.4460859189453788
+	 * 
+	 * so,now we know categories for yet unseen documents
+	 */
     }
 
-    void makeParagraphVectors()  throws Exception {
-      ClassPathResource resource = new ClassPathResource("paravec/labeled");
+    LabelAwareIterator iterator;
 
-      // build a iterator for our dataset
-      iterator = new FileLabelAwareIterator.Builder()
-              .addSourceFolder(resource.getFile())
-              .build();
+    ParagraphVectors paragraphVectors;
 
-      tokenizerFactory = new DefaultTokenizerFactory();
-      tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
-
-      // ParagraphVectors training configuration
-      paragraphVectors = new ParagraphVectors.Builder()
-              .learningRate(0.025)
-              .minLearningRate(0.001)
-              .batchSize(1000)
-              .epochs(20)
-              .iterate(iterator)
-              .trainWordVectors(true)
-              .tokenizerFactory(tokenizerFactory)
-              .build();
-
-      // Start model training
-      paragraphVectors.fit();
-    }
+    TokenizerFactory tokenizerFactory;
 
     void checkUnlabeledData() throws FileNotFoundException {
-      /*
-      At this point we assume that we have model built and we can check
-      which categories our unlabeled document falls into.
-      So we'll start loading our unlabeled documents and checking them
-     */
-     ClassPathResource unClassifiedResource = new ClassPathResource("paravec/unlabeled");
-     FileLabelAwareIterator unClassifiedIterator = new FileLabelAwareIterator.Builder()
-             .addSourceFolder(unClassifiedResource.getFile())
-             .build();
+	/*
+	 * At this point we assume that we have model built and we can check which
+	 * categories our unlabeled document falls into. So we'll start loading our
+	 * unlabeled documents and checking them
+	 */
+	final ClassPathResource unClassifiedResource = new ClassPathResource("paravec/unlabeled");
+	final FileLabelAwareIterator unClassifiedIterator = new FileLabelAwareIterator.Builder()//
+		.addSourceFolder(unClassifiedResource.getFile())//
+		.build();
 
-     /*
-      Now we'll iterate over unlabeled data, and check which label it could be assigned to
-      Please note: for many domains it's normal to have 1 document fall into few labels at once,
-      with different "weight" for each.
-     */
-     MeansBuilder meansBuilder = new MeansBuilder(
-         (InMemoryLookupTable<VocabWord>)paragraphVectors.getLookupTable(),
-           tokenizerFactory);
-     LabelSeeker seeker = new LabelSeeker(iterator.getLabelsSource().getLabels(),
-         (InMemoryLookupTable<VocabWord>) paragraphVectors.getLookupTable());
+	/*
+	 * Now we'll iterate over unlabeled data, and check which label it could be
+	 * assigned to Please note: for many domains it's normal to have 1 document fall
+	 * into few labels at once, with different "weight" for each.
+	 */
+	final MeansBuilder meansBuilder = new MeansBuilder(
+		(InMemoryLookupTable<VocabWord>) this.paragraphVectors.getLookupTable(), this.tokenizerFactory);
+	final LabelSeeker seeker = new LabelSeeker(this.iterator.getLabelsSource().getLabels(),
+		(InMemoryLookupTable<VocabWord>) this.paragraphVectors.getLookupTable());
 
-     while (unClassifiedIterator.hasNextDocument()) {
-         LabelledDocument document = unClassifiedIterator.nextDocument();
-         INDArray documentAsCentroid = meansBuilder.documentAsVector(document);
-         List<Pair<String, Double>> scores = seeker.getScores(documentAsCentroid);
+	while (unClassifiedIterator.hasNextDocument()) {
+	    final LabelledDocument document = unClassifiedIterator.nextDocument();
+	    final INDArray documentAsCentroid = meansBuilder.documentAsVector(document);
+	    final List<Pair<String, Double>> scores = seeker.getScores(documentAsCentroid);
 
-         /*
-          please note, document.getLabel() is used just to show which document we're looking at now,
-          as a substitute for printing out the whole document name.
-          So, labels on these two documents are used like titles,
-          just to visualize our classification done properly
-         */
-         log.info("Document '" + document.getLabel() + "' falls into the following categories: ");
-         for (Pair<String, Double> score: scores) {
-             log.info("        " + score.getFirst() + ": " + score.getSecond());
-         }
-     }
+	    /*
+	     * please note, document.getLabel() is used just to show which document we're
+	     * looking at now, as a substitute for printing out the whole document name. So,
+	     * labels on these two documents are used like titles, just to visualize our
+	     * classification done properly
+	     */
+	    ParagraphVectorsClassifierExample.log
+		    .info("Document '" + document.getLabel() + "' falls into the following categories: ");
+	    for (final Pair<String, Double> score : scores) {
+		ParagraphVectorsClassifierExample.log.info("        " + score.getFirst() + ": " + score.getSecond());
+	    }
+	}
 
+    }
+
+    void makeParagraphVectors() throws Exception {
+	final ClassPathResource resource = new ClassPathResource("paravec/labeled");
+
+	// build a iterator for our dataset
+	this.iterator = new FileLabelAwareIterator.Builder().addSourceFolder(resource.getFile()).build();
+
+	this.tokenizerFactory = new DefaultTokenizerFactory();
+	this.tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
+
+	// ParagraphVectors training configuration
+	this.paragraphVectors = new ParagraphVectors.Builder()//
+		.learningRate(0.025)//
+		.minLearningRate(0.001)//
+		.batchSize(1000)//
+		.epochs(20)//
+		.iterate(this.iterator)//
+		.trainWordVectors(true)//
+		.tokenizerFactory(this.tokenizerFactory)//
+		.build();
+
+	// Start model training
+	this.paragraphVectors.fit();
     }
 }
--- dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/Word2VecRawTextExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/Word2VecRawTextExample.java
@@ -1,5 +1,7 @@
 package org.deeplearning4j.examples.nlp.word2vec;
 
+import java.util.Collection;
+
 import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
@@ -8,65 +10,64 @@ import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
 import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Collection;
 
 /**
  * Created by agibsonccc on 10/9/14.
  *
- * Neural net that processes text into wordvectors. See below url for an in-depth explanation.
- * https://deeplearning4j.org/word2vec.html
+ * Neural net that processes text into wordvectors. See below url for an
+ * in-depth explanation. https://deeplearning4j.org/word2vec.html
  */
 public class Word2VecRawTextExample {
 
     private static Logger log = LoggerFactory.getLogger(Word2VecRawTextExample.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
 
-        // Gets Path to Text file
-        String filePath = new ClassPathResource("raw_sentences.txt").getFile().getAbsolutePath();
+	// Gets Path to Text file
+	final String filePath = new ClassPathResource("raw_sentences.txt").getFile().getAbsolutePath();
 
-        log.info("Load & Vectorize Sentences....");
-        // Strip white space before and after for each line
-        SentenceIterator iter = new BasicLineIterator(filePath);
-        // Split on white spaces in the line to get words
-        TokenizerFactory t = new DefaultTokenizerFactory();
+	Word2VecRawTextExample.log.info("Load & Vectorize Sentences....");
+	// Strip white space before and after for each line
+	final SentenceIterator iter = new BasicLineIterator(filePath);
+	// Split on white spaces in the line to get words
+	final TokenizerFactory t = new DefaultTokenizerFactory();
 
-        /*
-            CommonPreprocessor will apply the following regex to each token: [\d\.:,"'\(\)\[\]|/?!;]+
-            So, effectively all numbers, punctuation symbols and some special symbols are stripped off.
-            Additionally it forces lower case for all tokens.
-         */
-        t.setTokenPreProcessor(new CommonPreprocessor());
+	/*
+	 * CommonPreprocessor will apply the following regex to each token:
+	 * [\d\.:,"'\(\)\[\]|/?!;]+ So, effectively all numbers, punctuation symbols and
+	 * some special symbols are stripped off. Additionally it forces lower case for
+	 * all tokens.
+	 */
+	t.setTokenPreProcessor(new CommonPreprocessor());
 
-        log.info("Building model....");
-        Word2Vec vec = new Word2Vec.Builder()
-                .minWordFrequency(5)
-                .iterations(1)
-                .layerSize(100)
-                .seed(42)
-                .windowSize(5)
-                .iterate(iter)
-                .tokenizerFactory(t)
-                .build();
+	Word2VecRawTextExample.log.info("Building model....");
+	final Word2Vec vec = new Word2Vec.Builder() //
+		.minWordFrequency(5)//
+		.iterations(1)//
+		.layerSize(100)//
+		.seed(42)//
+		.windowSize(5)//
+		.iterate(iter)//
+		.tokenizerFactory(t)//
+		.build();//
 
-        log.info("Fitting Word2Vec model....");
-        vec.fit();
+	Word2VecRawTextExample.log.info("Fitting Word2Vec model....");
+	vec.fit();
 
-        log.info("Writing word vectors to text file....");
+	Word2VecRawTextExample.log.info("Writing word vectors to text file....");
 
-        // Write word vectors to file
-        WordVectorSerializer.writeWordVectors(vec, "pathToWriteto.txt");
+	// Write word vectors to file
+	WordVectorSerializer.writeWordVectors(vec, "pathToWriteto.txt");
 
-        // Prints out the closest 10 words to "day". An example on what to do with these Word Vectors.
-        log.info("Closest Words:");
-        Collection<String> lst = vec.wordsNearest("day", 10);
-        System.out.println("10 Words closest to 'day': " + lst);
+	// Prints out the closest 10 words to "day". An example on what to do with these
+	// Word Vectors.
+	Word2VecRawTextExample.log.info("Closest Words:");
+	final Collection<String> lst = vec.wordsNearest("day", 10);
+	System.out.println("10 Words closest to 'day': " + lst);
 
-        // TODO resolve missing UiServer
+	// TODO resolve missing UiServer
 //        UiServer server = UiServer.getInstance();
 //        System.out.println("Started on port " + server.getPort());
     }
